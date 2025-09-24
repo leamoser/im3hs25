49ff72565bd5978e1e364c585a00067b850fb985
Lea Moser <privat@lea-moser.ch>
2025-09-24 23:24:23 +0200

    write E (extract) from ETL

---
+++ backend/extract.php
@@ -0,0 +1,34 @@
+<?php
+
+// -> fetch von wetter api
+function fetchWeatherData() {
+    $url = "https://api.open-meteo.com/v1/forecast?latitude=47.42572213487477&longitude=9.37629353114485&current=temperature_2m";
+
+    $ch = curl_init($url);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    $response = curl_exec($ch);
+    curl_close($ch);
+
+    return json_decode($response, true);
+}
+
+
+// -> fetch von api, welche passant:innen daten zurückgibt
+function fetchPassengerData() {
+    $url = "https://daten.stadt.sg.ch/api/explore/v2.1/catalog/datasets/fussganger-stgaller-innenstadt-vadianstrasse/records?order_by=measured_at_new%20DESC&limit=1";
+
+    $ch = curl_init($url);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    $response = curl_exec($ch);
+    curl_close($ch);
+
+    return json_decode($response, true);
+}
+
+
+return [
+    'weather' => fetchWeatherData(),
+    'passengers' => fetchPassengerData()
+];
+
+?>
