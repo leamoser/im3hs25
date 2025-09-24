ed94f3bae677055637d293629eced32f380c7300
Lea Moser <privat@lea-moser.ch>
2025-09-24 23:45:18 +0200

    write L (transform) from ETL

---
+++ backend/load.php
@@ -0,0 +1,23 @@
+<?php
+
+$data = include('transform.php');
+
+// daten in einen array umwandeln
+$data = json_decode($data, true);
+
+// -> datenbank zugangsdaten einbinden
+require_once 'config.php';
+
+// -> verbindung mit der datenbank
+try {
+    $pdo = new PDO($dsn, $username, $password, $options);
+    $sql = "INSERT INTO entries (passengers, temperature) VALUES (?, ?)";
+    $stmt = $pdo->prepare($sql);
+    $stmt->execute([
+        $data['passengers'],
+        $data['temperature']
+    ]);
+    echo "Daten erfolgreich eingefügt.";
+} catch (PDOException $e) {
+    die("Verbindung zur Datenbank konnte nicht hergestellt werden: " . $e->getMessage());
+}
